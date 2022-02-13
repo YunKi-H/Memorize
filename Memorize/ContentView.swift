0c7a9d80e342485116ccb75d49baeb89bfa73d80
maemi <dbsrl759@naver.com>
2022-02-14 04:17:15 +0900

    Stanford_SwiftUI_lecture_1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        RoundedRectangle(cornerRadius: 25.0)
-            .stroke(lineWidth: 3)
-            .padding(.horizontal)
+        ZStack() {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3)
+            
+            Text("Hello, world!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
     }
 }
 
