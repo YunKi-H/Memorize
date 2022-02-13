6e5ff98055dd2c4d046b235d6b7d30ff5dbc28a6
maemi <dbsrl759@naver.com>
2022-02-13 23:51:42 +0900

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 매미 on 2022/02/13.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
